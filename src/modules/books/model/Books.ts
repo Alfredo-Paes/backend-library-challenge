3dc28e0e5f8d58bb09171fa38d8e9ee7e5b98e8e
Alfredo Paes <alfredo.alfpaes@gmail.com>
2021-10-02 16:54:23 -0300

    feat: model referring to books was created

---
+++ src/modules/books/model/Books.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4 } from 'uuid';
+
+class Books {
+  id?: string;
+
+  title: string;
+
+  publishing_company: string;
+
+  picture: string;
+
+  authors: string[];
+
+  created_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Books };
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const PORT = 3333;
@@ -6,5 +6,5 @@ const PORT = 3333;
 app.use(express.json());
 
 app.listen(PORT, () =>
-  console.log(`Everthing ok! Server is running on port:${PORT}!`)
+  console.log(`Everthing ok! Server is running on port:${PORT}!`),
 );
